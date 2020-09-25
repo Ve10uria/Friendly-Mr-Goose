c47619ec83ae5738e111a770c6db38cc10f814f6
Ve10uria <71888026+Ve10uria@users.noreply.github.com>
2020-09-25 11:03:51 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,182 @@
+tiles.setTilemap(tiles.createTilemap(hex`1000100002010101010104010304020202020402020302020203040102040202020204020202030302020102010402020404040203020202020202030204020202020402040404040404040102040201010104040202030101010202020404040102020102020202020202020202020403020202010202020202030101020304040402030404040404040403030202020202020302020202020204020202010303020203010301030101040202030202040101010203030203010203010102020404040402020202020202020203020302020202020302020103040404040402020302010202030202020302020104020203020202020202020202020302040202020202`, img`
+    . . . . . . 2 . . 2 . . . . 2 . 
+    . . . . . . 2 . . 2 . . . . 2 . 
+    . . . . . . . . . 2 . . 2 2 2 . 
+    . . . . . . . . . 2 . . . . 2 . 
+    2 2 2 2 2 2 2 . . 2 . . . . 2 2 
+    . . . . . . . . . 2 2 2 . . . . 
+    . . . . . . . . . . . 2 . . . . 
+    . . . . . . . . . . . 2 2 2 . . 
+    2 2 2 2 2 2 2 . . . . . . . . . 
+    . . . . . . 2 . . . . . . . . . 
+    . . . . . . 2 . . . . . 2 . . . 
+    . . . . . . . . . . . . 2 2 2 2 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 2 2 2 2 2 . . . . . 
+    . . . . . . . . . . 2 . . . . . 
+    . . . . . . . . . . 2 . . . . . 
+    `, [myTiles.transparency16,sprites.castle.tileGrass3,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.builtin.brick], TileScale.Sixteen))
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . f f f . . 
+    . . . . . . . . . . . f 1 f . . 
+    . . . . . . . . . . . f 1 f f . 
+    . . . . . . . . . . . f f . . . 
+    . . . . . . . . . . . f f . . . 
+    . . . . . . . e e e e d f . . . 
+    . . . . e e e e e e e d d d . . 
+    . . 1 e e e e e e e d d d d . . 
+    . . . 1 1 e e e e d d d d d . . 
+    . . . . . 1 d d d d d d . . . . 
+    . . . . . . . f . . f . . . . . 
+    . . . . . . . f . . f . . . . . 
+    . . . . . . . f f . f f . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite, 50, 50)
+mySprite.setPosition(14, 115)
+let myEnemy = sprites.create(img`
+    . . . . f f f f . . . . . 
+    . . f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f f f c c f f f c . 
+    f f f c f f f f f f f c . 
+    c c c f f f e e f f c c . 
+    f f f f f e e f f c c f . 
+    f f f b f e e f b f f f . 
+    . f 4 1 f 4 4 f 1 4 f . . 
+    . f e 4 4 4 4 4 4 e f . . 
+    . f f f e e e e f f f . . 
+    f e f b 7 7 7 7 b f e f . 
+    e 4 f 7 7 7 7 7 7 f 4 e . 
+    e e f 6 6 6 6 6 6 f e e . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Enemy)
+myEnemy.setPosition(80, 45)
+let Enemy2 = sprites.create(img`
+    . f f f . f f f f . f f f . 
+    f f f f f c c c c f f f f f 
+    f f f f b c c c c b f f f f 
+    f f f c 3 c c c c 3 c f f f 
+    . f 3 3 c c c c c c 3 3 f . 
+    . f c c c c d d c c c c f . 
+    . f f c c d d d d c c f f . 
+    . f f f b f d d f b f f f . 
+    . f f d 1 f d d f 1 d f f . 
+    . . f f d d d d d d f f . . 
+    . . e f e d d d d e f e . . 
+    . e d f b 3 3 3 3 b f d e . 
+    . d d f 3 3 3 3 3 3 c d d . 
+    . d d f 6 6 6 6 6 6 f d d . 
+    . . . . f f f f f f . . . . 
+    . . . . f f . . f f . . . . 
+    `, SpriteKind.Enemy)
+Enemy2.setPosition(22, 19)
+let Enemy3 = sprites.create(img`
+    . . . . 5 5 5 5 . . . . . 
+    . . 5 5 5 5 5 5 5 5 . . . 
+    . 5 5 5 5 5 5 1 5 5 5 . . 
+    5 5 5 5 5 5 1 1 5 5 5 1 . 
+    5 5 5 1 5 5 5 5 5 5 5 1 . 
+    1 1 1 5 5 5 b b 5 5 1 1 . 
+    5 5 5 5 f b b f 5 1 1 5 . 
+    5 5 5 b f b b f b 5 5 5 . 
+    . f d 1 f d d f 1 d f . . 
+    . f b d d d d d d b f . . 
+    . f f f b b b b f f f . . 
+    f b f b 3 3 3 3 b f b f . 
+    b d f 3 3 3 3 3 3 f d b . 
+    b b f 6 6 6 6 6 6 f b b . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Enemy)
+Enemy3.setPosition(20, 200)
+let Enemy4 = sprites.create(img`
+    . . . . 9 9 9 9 . . . . . 
+    . . 9 9 9 9 9 9 9 9 . . . 
+    . 9 9 9 9 9 9 6 9 9 9 . . 
+    9 9 9 9 9 9 6 6 9 9 9 6 . 
+    9 9 9 6 9 9 9 9 9 9 9 6 . 
+    6 6 6 9 9 9 e e 9 9 6 6 . 
+    9 9 9 9 f e e f 9 9 6 9 . 
+    9 9 9 b f e e f b f f 9 . 
+    9 f e 1 f e e f 1 e f 9 . 
+    9 f e e e e e e e e f 9 . 
+    9 f f f e e e e f f f 9 . 
+    f e f b 7 7 7 7 b f e f . 
+    e e f 7 7 7 7 7 7 f e e . 
+    e e f 6 6 6 6 6 6 f e e . 
+    . . 9 f f f f f f 9 . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Enemy)
+Enemy4.setPosition(100, 190)
+let Enemy5 = sprites.create(img`
+    . . . . 4 4 4 4 . . . . . 
+    . . 4 4 4 4 4 4 4 4 . . . 
+    . 4 4 4 4 4 4 4 4 4 4 . . 
+    4 4 4 4 4 4 4 4 4 4 4 4 . 
+    4 4 4 4 4 4 4 4 4 4 4 4 . 
+    4 4 4 4 4 4 d d 4 4 4 4 . 
+    4 4 4 4 f d d f 4 4 4 4 . 
+    4 4 d b f d d f 4 4 4 4 . 
+    4 f d 1 f d d f 1 d f 4 . 
+    . f d 3 3 d d 3 3 d f . . 
+    . f f f d d d d f f f . . 
+    f d f b 9 9 9 9 b f d f . 
+    d d f 9 9 9 9 9 9 f d d . 
+    d d f 8 8 8 8 8 8 f d d . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Enemy)
+Enemy5.setPosition(140, 240)
+let Enemy6 = sprites.create(img`
+    . . . . f f f f . . . . . 
+    . . f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f f f c c f f f c . 
+    f f f c f f f f f f f c . 
+    c c c f f f d d f f c c . 
+    f f f f f d d f f c c f . 
+    f f f b f d d f b f f f . 
+    f f d 1 f d d f 1 d f f . 
+    f f d d d 2 2 d d d f f . 
+    f f f f d d d d f f f f . 
+    f d f 2 2 d d 2 2 f d f . 
+    d d f 2 2 2 2 2 2 f d d . 
+    d d f 2 2 2 2 2 2 f d d . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+Enemy6.setPosition(200, 17)
+let Enemy7 = sprites.create(img`
+    . . . . e e e e . . . . . 
+    . . e e e e e e e e . . . 
+    . e e e e e e e e e e . . 
+    e e e e e e e e e e e e . 
+    e e e e e e e e e e e e . 
+    e e e e e e d d e e e e . 
+    e e e e f d d f e e e e . 
+    e e e b f d d f b e e e . 
+    e e d 1 f d d f 1 d e e . 
+    e e d d d d d d d d e e . 
+    e e e e d d d d e e e e . 
+    e d f b 4 4 4 4 b f d e . 
+    d d f 4 4 4 4 4 4 f d d . 
+    d d f 6 6 6 6 6 6 f d d . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+Enemy7.setPosition(240, 240)
+forever(function () {
+    if (mySprite.overlapsWith(myEnemy)) {
+        if (controller.A.isPressed()) {
+            myEnemy.destroy(effects.spray, 500)
+        }
+    }
+    if (true) {
+    	
+    }
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`1000100002010101010104010304020202020402020302020203040102040202020204020202030302020102010402020404040203020202020202030204020202020402040404040404040102040201010104040202030101010202020404040102020102020202020202020202020403020202010202020202030101020304040402030404040404040403030202020202020302020202020204020202010303020203010301030101040202030202040101010203030203010203010102020404040402020202020202020203020302020202020302020103040404040402020302010202030202020302020104020203020202020202020202020302040202020202`, img`
+. . . . . . 2 . . 2 . . . . 2 . 
+. . . . . . 2 . . 2 . . . . 2 . 
+. . . . . . . . . 2 . . 2 2 2 . 
+. . . . . . . . . 2 . . . . 2 . 
+2 2 2 2 2 2 2 . . 2 . . . . 2 2 
+. . . . . . . . . 2 2 2 . . . . 
+. . . . . . . . . . . 2 . . . . 
+. . . . . . . . . . . 2 2 2 . . 
+2 2 2 2 2 2 2 . . . . . . . . . 
+. . . . . . 2 . . . . . . . . . 
+. . . . . . 2 . . . . . 2 . . . 
+. . . . . . . . . . . . 2 2 2 2 
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 2 2 2 2 . . . . . 
+. . . . . . . . . . 2 . . . . . 
+. . . . . . . . . . 2 . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass3,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.builtin.brick], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
